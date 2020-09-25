d33d22428c1cde3b6725154f91d505aa8c00fc5d
Positron Robotics <positronrobotics@gmail.com>
2020-09-25 20:18:54 +0530

    chumma

--- Blink_Try1.ino
+++ Blink_Try1.ino
@@ -3,7 +3,11 @@
 
 #define SERVOSHOULDERRIGHTFRONT_POS_HOME 38
 #define SERVOSHOULDERRIGHTLATERAL_POS_HOME 159
-#define SERVOELBOWRIGHT 103
+#define SERVOELBOWRIGHT_POS_HOME 103
+
+#define UNDEFINED 9
+#define MOVING_TO_HOME_POS 0
+#define MOVED_TO_HOME_POS 1
 
 Servo servoShoulderRightFrontal;
 Servo servoShoulderRightLateral;
@@ -11,9 +15,11 @@ Servo servoElbowRight;
 
 int servoShoulderRightFrontal_pos = SERVOSHOULDERRIGHTFRONT_POS_HOME;
 int servoShoulderRightLateral_pos = SERVOSHOULDERRIGHTLATERAL_POS_HOME;
-int servoElbowRight_pos = SERVOELBOWRIGHT;
+int servoElbowRight_pos = SERVOELBOWRIGHT_POS_HOME;
 
-int m1=0;
+int curr_m1=0;
+int prev_m1=0;
+int move_home_process=UNDEFINED;
 
 // create servo object to control a servo
 // twelve servo objects can be created on most boards
@@ -32,7 +38,23 @@ void setup()
 
 void loop()
 {
-  delay(1);
+  if((prev_m1==0) && (curr_m1==1))
+  {
+    move_home_process=MOVING_TO_HOME_POS;    
+    prev_m1=curr_m1;
+
+    //servoElbowRight.write(SERVOELBOWRIGHT_POS_HOME);
+    Serial.println("servoElbowRight");
+    delay(5);
+    //servoShoulderRightLateral_pos.write(SERVOSHOULDERRIGHTLATERAL_POS_HOME);
+    Serial.println("servoShoulderRightLateral_pos");
+    delay(5);
+    //servoShoulderRightFrontal.write(SERVOSHOULDERRIGHTFRONT_POS_HOME);
+    Serial.println("servoShoulderRightFrontal");
+    delay(5);
+
+    move_home_process=MOVED_TO_HOME_POS;
+  }
 }
 
 // function that executes whenever data is received from master
@@ -42,6 +64,7 @@ void receiveEvent(int howMany)
 
   char angleStr[4];
   int angle=0;
+  int m1=0;
   
   while (0 <Wire.available())
   {
@@ -57,26 +80,35 @@ void receiveEvent(int howMany)
     Serial.print("m1=");
     Serial.print(m1);
     Serial.println();
+    
+    if(move_home_process!=MOVING_TO_HOME_POS)
+    {
+      Serial.println("Assigned to curr_m1");
+      curr_m1=m1;      
+    }
   }
   else
   {
-    angleStr[0]=rcmd[0];
-    angleStr[1]=rcmd[1];
-    angleStr[2]=rcmd[2];
-    angleStr[3]=rcmd[3];
-  
-    Serial.print("angleStr:");
-    Serial.print(angleStr);
-    Serial.println();  
-  
-    Serial.print("angle:");
-    sscanf(angleStr,"%04d",&angle);
-    Serial.print(angle);
-  
-    servoShoulderRightFrontal.write(angle);
+    if(move_home_process==MOVED_TO_HOME_POS)
+    {    
+      angleStr[0]=rcmd[0];
+      angleStr[1]=rcmd[1];
+      angleStr[2]=rcmd[2];
+      angleStr[3]=rcmd[3];
     
-    //Serial.print(rcmd.substring(3,4));
-    Serial.println();             /* to newline */
+      Serial.print("angleStr:");
+      Serial.print(angleStr);
+      Serial.println();  
+    
+      Serial.print("angle:");
+      sscanf(angleStr,"%04d",&angle);
+      Serial.print(angle);
+    
+      //servoShoulderRightFrontal.write(angle);
+      
+      //Serial.print(rcmd.substring(3,4));
+      Serial.println();
+    }
   }
 
   //
