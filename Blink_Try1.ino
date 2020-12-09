e42fdfad2efb9565b20d844a91856ac5aca6a5a0
Positron Robotics <positronrobotics@gmail.com>
2020-12-09 17:47:50 +0530

    just

--- Blink_Try1.ino
+++ Blink_Try1.ino
@@ -10,6 +10,14 @@
 #define MOVING_TO_HOME_POS 0
 #define MOVED_TO_HOME_POS 1
 
+#define VEHICLE_STOPPED 0
+#define VEHICLE_MOVING_FWD 1
+#define VEHICLE_MOVING_BCK 2
+
+#define VEHICLE_TURN_STRAIGHT 0
+#define VEHICLE_TURN_LEFT 1
+#define VEHICLE_TURN_RIGHT 2
+
 Servo servoShoulderRightFrontal;
 Servo servoShoulderRightLateral;
 Servo servoElbowRight;
@@ -25,6 +33,13 @@ int move_home_process=UNDEFINED;
 AF_DCMotor motorLeft(4);
 AF_DCMotor motorRight(3);
 
+int vehicle_state;
+int vehicle_turn_state;
+int vehicle_state_in_transition; 
+
+int vehicle_leftSpeed=0;
+int vehicle_rightSpeed=0;
+
 // create servo object to control a servo
 // twelve servo objects can be created on most boards
 
@@ -36,7 +51,13 @@ void setup()
   Wire.onReceive(receiveEvent);
 
   motorLeft.run(RELEASE);
-  motorRight.run(RELEASE);  
+  motorRight.run(RELEASE);
+
+  vehicle_state=VEHICLE_STOPPED;
+  vehicle_turn_state=VEHICLE_TURN_STRAIGHT;
+  vehicle_state_in_transition=0;
+
+  Serial.println("check1");
 }
 
 void loop()
@@ -63,6 +84,11 @@ void loop()
 
     move_home_process=MOVED_TO_HOME_POS;
   }
+
+  if(vehicle_state_in_transition==1)
+  {
+        
+  }
 }
 
 // function that executes whenever data is received from master
@@ -73,9 +99,6 @@ void receiveEvent(int howMany)
   char angleStr[4];
   int angle=0;
   int m1=0;
-
-  int leftMotorSpeed=0;
-  int rightMotorSpeed=0;
 
   int joystick_x=0,joystick_y=0;
   char joystick_xy[1];
@@ -103,18 +126,38 @@ void receiveEvent(int howMany)
   }
   else if((rcmd[0]=='j') && (rcmd[1]=='1'))
   {
-    joystick_xy[0]=rcmd[2];
-    sscanf(joystick_xy,"%01d",&joystick_x);
-    
-    joystick_xy[0]=rcmd[3];
-    sscanf(joystick_xy,"%01d",&joystick_y);
+    if(vehicle_state_in_transition==0)
+    {
+      joystick_xy[0]=rcmd[2];
+      sscanf(joystick_xy,"%01d",&joystick_x);
+      
+      joystick_xy[0]=rcmd[3];
+      sscanf(joystick_xy,"%01d",&joystick_y);
+  
+      Serial.print("joystick_x=");
+      Serial.print(joystick_x); 
+  
+      Serial.print("joystick_y=");
+      Serial.print(joystick_y);
+      Serial.println();
+  
+      if((joystick_x>=3) || (joystick_x<=7))
+      {
+        vehicle_turn_state=VEHICLE_TURN_STRAIGHT;
+      }
+      else if(joystick_x<3)
+      {
+        vehicle_turn_state=VEHICLE_TURN_LEFT;
+      }
+      else if(joystick_x>7)
+      {
+        vehicle_turn_state=VEHICLE_TURN_RIGHT;
+      }
 
-    Serial.print("joystick_x=");
-    Serial.print(joystick_x); 
-
-    Serial.print("joystick_y=");
-    Serial.print(joystick_y);
-    Serial.println(); 
+      //if(joystick_y>=5
+  
+      //vehicle_state_in_transition=1;
+    }
   }
   else
   {
