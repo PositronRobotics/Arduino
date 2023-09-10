a3311d99437545656d81bb8d695d75e263db5a87
Positron Robotics <positronrobotics@gmail.com>
2023-09-10 15:53:06 +0530

    starting merging servo here

--- Blink_Try1.ino
+++ Blink_Try1.ino
@@ -18,6 +18,20 @@ int KeepAlive_came=0;
 AF_DCMotor motorLeft(4);
 AF_DCMotor motorRight(3);
 
+Servo servoShoulderLeftLateral;
+Servo servoShoulderLeftFrontal;
+Servo servoElbowLeft;
+
+int servoShoulderLeftLateral_pos = 41;
+int servoShoulderLeftFrontal_pos = 109;
+int servoElbowLeft_pos = 67;
+
+int prev_servoShoulderLeftLateral_pos = 0;
+int prev_servoShoulderLeftFrontal_pos = 0;
+int prev_servoElbowLeft_pos = 0;
+
+char serInput=0;
+
 void setup()
 {
   Serial.begin(115200);
@@ -29,6 +43,14 @@ void setup()
   motorLeft.run(RELEASE);
   motorRight.setSpeed(255); 
   motorRight.run(RELEASE);  
+  
+  servoShoulderLeftLateral.attach(9);  // attaches the servo on pin 9 to the servo object
+  servoShoulderLeftFrontal.attach(10);
+  servoElbowLeft.attach(11);
+  
+  servoShoulderLeftLateral.write(servoShoulderLeftLateral_pos);
+  servoShoulderLeftFrontal.write(servoShoulderLeftFrontal_pos);
+  servoElbowLeft.write(servoElbowLeft_pos);  
 
   Wire.begin(8);
   Wire.onReceive(receiveEvent);
@@ -125,6 +147,86 @@ void loop()
       }
     }
   }
+  
+  if (Serial.available() > 0)
+  {
+    serInput=0;
+    serInput = Serial.read();
+
+    Serial.print("Received:");
+    Serial.println(serInput);
+
+    if((serInput==97)||(serInput==122)||(serInput==115)||(serInput==120)||(serInput==100)||(serInput==99))
+    {
+      if(serInput==97)
+      {
+        if(servoShoulderLeftLateral_pos<180)
+        {
+          servoShoulderLeftLateral_pos++;
+        }
+      }
+      else if(serInput==122)
+      {
+        if(servoShoulderLeftLateral_pos>0)
+        {
+          servoShoulderLeftLateral_pos--;
+        }
+      }
+      else if(serInput==115)
+      {
+        if(servoShoulderLeftFrontal_pos<180)
+        {
+          servoShoulderLeftFrontal_pos++;
+        }
+      }
+      else if(serInput==120)
+      {
+        if(servoShoulderLeftFrontal_pos>0)
+        {
+          servoShoulderLeftFrontal_pos--;
+        }
+      }
+      else if(serInput==100)
+      {
+        if(servoElbowLeft_pos<180)
+        {
+          servoElbowLeft_pos++;
+        }
+      }
+      else if(serInput==99)
+      {
+        if(servoElbowLeft_pos>0)
+        {
+          servoElbowLeft_pos--;
+        }
+      }
+
+      Serial.print("servoShoulderLeftLateral_pos:");
+      Serial.print(servoShoulderLeftLateral_pos);
+      Serial.print(" ;servoShoulderLeftFrontal_pos:");
+      Serial.print(servoShoulderLeftFrontal_pos);
+      Serial.print(" ;servoElbowLeft_pos:");
+      Serial.println(servoElbowLeft_pos);            
+
+      if(prev_servoShoulderLeftLateral_pos!=servoShoulderLeftLateral_pos)
+      {
+        prev_servoShoulderLeftLateral_pos=servoShoulderLeftLateral_pos;
+        servoShoulderLeftLateral.write(servoShoulderLeftLateral_pos);
+      }
+
+      if(prev_servoShoulderLeftFrontal_pos!=servoShoulderLeftFrontal_pos)
+      {
+        prev_servoShoulderLeftFrontal_pos=servoShoulderLeftFrontal_pos;
+        servoShoulderLeftFrontal.write(servoShoulderLeftFrontal_pos);
+      }  
+
+      if(prev_servoElbowLeft_pos!=servoElbowLeft_pos)
+      {
+        prev_servoElbowLeft_pos=servoElbowLeft_pos;
+        servoElbowLeft.write(servoElbowLeft_pos);
+      }  
+    }  
+	}
 }
 
 // function that executes whenever data is received from master
@@ -200,7 +302,5 @@ void receiveEvent(int howMany)
     }
 
     trig=1;
-  }
-
-    
+  }    
 }
